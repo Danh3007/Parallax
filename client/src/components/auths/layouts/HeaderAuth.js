0a0d16eea97e21ff93a24f5df80fbb00f2881966
DanhGaST <kieudanh.itc@gmail.com>
2021-08-15 20:01:17 +0700

    back-end list product

--- client/src/components/auths/layouts/HeaderAuth.js
+++ client/src/components/auths/layouts/HeaderAuth.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"; // eslint-disable-line
 
-import Logo from "./../../../images/logo.png"; // eslint-disable-line
 class HeaderAuth extends Component {
   render() {
     return (
       <header className="headerAuth">
         <div className="container">
           <Link to="/">
-            <img src={Logo} alt="logo-shop" className="logoAuth"></img>
+            <img src="./images/logo.png" title="Logo" alt="logo-shop" className="logoAuth"></img>
           </Link>
           <span className="txtAuth">Đăng Nhập / Đăng Kí</span>
-        </div>
+        </div>  
       </header>
     );
   }
--- serve/router/index.js
+++ serve/router/index.js
@@ -1,8 +1,10 @@
 
 const auth = require("./auth")
+const myHome = require("./myHome")
 
 function route(app) {
     app.use('/auth', auth)
+    app.use('/', myHome)
 }
 
 module.exports = route
